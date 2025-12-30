6b21881ad2d651eb646cbd7e68365b2e8b21fa0c
Pratik Singh <pratikkumars623@gmail.com>
2025-12-30 23:16:46 +0530

    database setup and models created

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,8 @@
-import { UserButton } from '@clerk/nextjs'
-import React from 'react'
 
 const Home = () => {
   return (
     <div>
       <p>Home</p>
-      <UserButton afterSignOutUrl='/'/>
     </div>
   )
 }
---
+++ lib/models/user.model.ts
@@ -0,0 +1,41 @@
+import { model, models, Schema } from "mongoose";
+
+const userSchema=new Schema({
+    clerkId:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    photo:{
+        type:String,
+        required:true,
+    },
+    firstName:{
+        type:String,
+    },
+    lastName:{
+        type:String,
+    },
+    planId:{
+        type:Number,
+        default:1,
+    },
+    creditBalance:{
+        type:Number,
+        default:1,
+    },
+
+});
+
+const User=models?.User || model("User",userSchema);
+export default User;
